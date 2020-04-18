6a390850fa4414b37655cbd0f956de9e214084c7
kitasota <kitasota123@gmail.com>
2020-04-18 18:19:51 +0900

    portfolio

---
+++ sample2.js
@@ -0,0 +1,30 @@
+jQuery(function() {
+  var pagetop = $('#page_top');   
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+  $('a[href^="#"]').click(function(){
+    //スクロールのスピード
+    var speed = 500;
+    //リンク元を取得
+    var href= $(this).attr("href");
+    //リンク先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    //リンク先までの距離を取得
+    var position = target.offset().top;
+    //スムーススクロール
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
